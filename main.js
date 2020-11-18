86382ed3b96a757fb743672f8af3640e8f8663f2
Ryukakusan1 <70544085+Ryukakusan1@users.noreply.github.com>
2020-11-18 10:29:59 -0600

    changes

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("What should I do?");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
@@ -53,4 +53,3 @@ function newElement() {
     }
   }
 }
-  </script>
